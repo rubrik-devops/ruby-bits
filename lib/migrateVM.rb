4e7a5e8e7487e44225f6e21c2add8c2dcea7b147
Peter J. Milanese <peterm@milanese.life>
2017-07-10 15:12:58 -0400

    ,,,-

--- lib/migrateVM.rb
+++ lib/migrateVM.rb
@@ -1,8 +1,9 @@
+require 'celluloid'
 
 class MigrateVM
-    include Celluloid
+  include Celluloid
 # Get vmWare Hosts for the Cluster, merging vcenter name into the hash
-    def migrate_vm(vmobj)
+  def migrate_vm(vmobj)
     vcenters=getFromApi("/api/v1/vmware/vcenter")
     vmwareVCenters = {}
     vcenters["data"].each do |vcenter|
@@ -21,46 +22,46 @@ class MigrateVM
     sla_hash = getSlaHash()
 
 #    vmlist.each do |vmobj|
-      logme("#{vmobj['VMName']}","Begin Workflow","Begin Workflow")
+    logme("#{vmobj['VMName']}","Begin Workflow","Begin Workflow")
 
 # Shutdown the VM and monitor to completion
-      shutdownVm(vmobj['fromVCenter'],@options.vcenteruser,@options.vcenterpw,vmobj['fromDatacenter'],vmobj['VMName'])
+    shutdownVm(vmobj['fromVCenter'],@options.vcenteruser,@options.vcenterpw,vmobj['fromDatacenter'],vmobj['VMName'])
 
 # Snapshot the VM and monitor to completion
-      id=findVmItem(vmobj['VMName'],'id')
-      effectiveSla = sla_hash[findVmItem(vmobj['VMName'], 'effectiveSlaDomainId')]
-      logme("#{vmobj['VMName']}","Request Snapshot",id)
-      snapshot_job = JSON.parse(setToApi('/api/v1/vmware/vm/' + id + '/snapshot','',"post"))['id']
-      logme("#{vmobj['VMName']}","Monitor Snapshot Request",snapshot_job)
-      snapshot_status = ''
-      last_snapshot_status = ''
-      while snapshot_status != "SUCCEEDED"
-        snapshot_status = getFromApi('/api/v1/vmware/vm/request/' + snapshot_job)['status']
-        if snapshot_status != last_snapshot_status
-          logme("#{vmobj['VMName']}","Monitor Snapshot",snapshot_status)
-        end
-        last_snapshot_status = snapshot_status
-        sleep(5)
+    id=findVmItem(vmobj['VMName'],'id')
+    effectiveSla = sla_hash[findVmItem(vmobj['VMName'], 'effectiveSlaDomainId')]
+    logme("#{vmobj['VMName']}","Request Snapshot",id)
+    snapshot_job = JSON.parse(setToApi('/api/v1/vmware/vm/' + id + '/snapshot','',"post"))['id']
+    logme("#{vmobj['VMName']}","Monitor Snapshot Request",snapshot_job)
+    snapshot_status = ''
+    last_snapshot_status = ''
+    while snapshot_status != "SUCCEEDED"
+      snapshot_status = getFromApi('/api/v1/vmware/vm/request/' + snapshot_job)['status']
+      if snapshot_status != last_snapshot_status
+        logme("#{vmobj['VMName']}","Monitor Snapshot",snapshot_status)
       end
+      last_snapshot_status = snapshot_status
+      sleep(5)
+    end
 
 # Perform Instant Recovery to new cluster hosts with poweredOff
-      h=getFromApi("/api/v1/vmware/vm/#{id}/snapshot")
-      latestSnapshot =  h['data'][0]['id']
-      logme("#{vmobj['VMName']}","Get Snapshot ID",latestSnapshot)
+    h=getFromApi("/api/v1/vmware/vm/#{id}/snapshot")
+    latestSnapshot =  h['data'][0]['id']
+    logme("#{vmobj['VMName']}","Get Snapshot ID",latestSnapshot)
 
 
-      logme("#{vmobj['VMName']}","Get VMWare Hosts from Rubrik","NOT DONE YET")
+    logme("#{vmobj['VMName']}","Get VMWare Hosts from Rubrik","NOT DONE YET")
 #o = setToApi('/api/v1/vmware/vm/snapshot/' + latestSnapshot + '/instant_recover',{ "vmName" => "#{@options.vm}","hostId" => "#{hostList[0]}","removeNetworkDevices" => true},"post")
 #puts '/api/v1/vmware/vm/snapshot/' + latestSnapshot + '/instant_recover'
 
 # Perform Instant Recovery
-      logme("#{vmobj['VMName']}","Request Instant Recovery","NOT DONE YET")
+    logme("#{vmobj['VMName']}","Request Instant Recovery","NOT DONE YET")
 
 # Swap the PortGroup (Don't know how this happens yet)
-      logme("#{vmobj['VMName']}","Change Port Group","NOT DONE YET")
+    logme("#{vmobj['VMName']}","Change Port Group","NOT DONE YET")
 
 # Start the VM
-      startVm(vmobj['toVCenter'],@options.vcenteruser,@options.vcenterpw,vmobj['toDatacenter'],vmobj['VMName'])
+    startVm(vmobj['toVCenter'],@options.vcenteruser,@options.vcenterpw,vmobj['toDatacenter'],vmobj['VMName'])
 
 # VMotion to production storage
     logme("#{vmobj['VMName']}","VMotion from Rubrik","NOT DONE YET")
